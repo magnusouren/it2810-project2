4ec991e5039c5f3ead2e3b5c46f4d702e7f4e74d
magnusouren <magnut0203@gmail.com>
2023-09-28 09:45:56 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    setupFiles: './setups.ts',
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+  base: '/project1',
+});
